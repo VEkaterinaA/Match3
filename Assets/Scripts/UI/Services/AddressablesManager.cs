c4d5d8bc621167def8a26302b5fdfa9d24befd8c
Ekaterina Agapova <astoriyavf2001@gmail.com>
2024-12-12 14:23:44 +0300

    Project context
    Added project context and project installer.
    Written resource and scene loading service.
    Added android provider.

---
+++ Assets/Scripts/UI/Services/AddressablesManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine.AddressableAssets;
+
+namespace UI.Services
+{
+	public class AddressablesManager
+	{
+		public AddressablesManager()
+		{
+			Addressables.InitializeAsync();
+		}
+	}
+}
